34257cf5aa6384a1f8963e7dd8ec2f8ed0b86d34
adham-source <adham.ahmad1988as@gmail.com>
2022-05-22 00:06:01 +0200

    Goal project

--- mern-rest-simple-API/backend/server.js
+++ mern-rest-simple-API/backend/server.js
@@ -19,10 +19,8 @@ app.use("/api/goals", require("./routes/goal"))
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-  app.get("/", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   })
 }
 
